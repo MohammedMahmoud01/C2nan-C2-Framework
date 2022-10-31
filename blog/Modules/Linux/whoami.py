9893a3bc8738f7d6318f1718ce9f1cd3926c07c1
Salma <salmaelkomy450@gmail.com>
2022-10-31 15:28:39 -0400

    some linux modules added

---
+++ blog/Modules/Linux/whoami.py
@@ -0,0 +1,15 @@
+import os
+from blog.views import *
+from blog.models import *
+
+current_path= os.path.dirname(os.path.abspath(__file__))
+
+def Whoami(request, agent=''):
+    if request.method=='POST':
+        task = "echo '==========whoami===========';whoami;echo '=========';echo 'Current user group:';id 2>/dev/null"
+        task_path = os.path.normpath(current_path+os.sep+os.pardir+os.sep+os.pardir)+"/data/listeners/agents/{}/tasks".format(agent)
+        with open(task_path, "w") as f:
+            f.write(task)
+            f.close()
+    else:
+        return render(request, 'blog/listeners.html')
