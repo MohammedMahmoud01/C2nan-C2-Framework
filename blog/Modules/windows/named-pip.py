15fbae7186e44670f2668856d0dbd1cae610d2e3
Abdelaziz Elhawary <abdelaziz.elhawary97@gmail.com>
2022-11-07 17:27:23 -0500

    named-pip module

--- blog/Modules/windows/named-pip.py
+++ blog/Modules/windows/named-pip.py
@@ -4,9 +4,9 @@ from blog.models import *
 
 current_path= os.path.dirname(os.path.abspath(__file__))
 
-def Applocker(request, agent=''):
+def Named_pipes(request, agent=''):
     if request.method=='POST':
-        task = 'echo "===============AppLocker Rules===============";Get-AppLockerPolicy -Effective | select -ExpandProperty RuleCollections;echo "===============AppLocker Policy Test===============";Get-AppLockerPolicy -Local | Test-AppLockerPolicy -path C:\Windows\System32\cmd.exe -User Everyone'
+        task = 'echo "===============List named pipes===============";cmd;pipelist.exe /accepteula'
         task_path = os.path.normpath(current_path+os.sep+os.pardir+os.sep+os.pardir)+"/data/listeners/agents/{}/tasks".format(agent)
         with open(task_path, "w") as f:
             f.write(task)
--- blog/Modules/windows/rev-per-pip.py
+++ blog/Modules/windows/rev-per-pip.py
@@ -4,9 +4,9 @@ from blog.models import *
 
 current_path= os.path.dirname(os.path.abspath(__file__))
 
-def Applocker(request, agent=''):
+def Perm_pip(request, agent='',named_pip_name=''):
     if request.method=='POST':
-        task = 'echo "===============AppLocker Rules===============";Get-AppLockerPolicy -Effective | select -ExpandProperty RuleCollections;echo "===============AppLocker Policy Test===============";Get-AppLockerPolicy -Local | Test-AppLockerPolicy -path C:\Windows\System32\cmd.exe -User Everyone'
+        task = 'echo "===============Review permissions on a named pipe===============";cmd;accesschk.exe /accepteula \\.\Pipe\{} -v'.format(named_pip_name)
         task_path = os.path.normpath(current_path+os.sep+os.pardir+os.sep+os.pardir)+"/data/listeners/agents/{}/tasks".format(agent)
         with open(task_path, "w") as f:
             f.write(task)
