cfbb0f700daf8c3beb6f9af53ff73cf12a26784d
Salma <salmaelkomy450@gmail.com>
2022-12-01 19:14:24 -0500

    Linux Modules Added

---
+++ blog/Modules/Linux/Apache.py
@@ -0,0 +1,17 @@
+import os
+from blog.views import *
+from blog.models import *
+
+current_path= os.path.dirname(os.path.abspath(__file__))
+
+def Apache(request, agent=''):
+    if request.method=='POST':
+        f = open("{}".format(current_path+"Apache-file"), "rt")
+        task = f.read()
+        f.close()
+        task_path = os.path.normpath(current_path+os.sep+os.pardir+os.sep+os.pardir)+"/data/listeners/agents/{}/tasks".format(agent)
+        with open(task_path, "w") as f:
+            f.write(task)
+            f.close()
+    else:
+        return render(request, 'blog/listeners.html')
