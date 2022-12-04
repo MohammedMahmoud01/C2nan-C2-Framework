cffa5c0abc99e6bc18aacd5cee87909a6e0b8383
root <you@example.com>
2022-12-04 17:09:51 -0500

    AD Modules

--- blog/Modules/Active-dir/ACCs-forKerb.py
+++ blog/Modules/Active-dir/ACCs-forKerb.py
@@ -4,10 +4,12 @@ from blog.models import *
 
 current_path= os.path.dirname(os.path.abspath(__file__))
 
+#### we need to download ../general/PowerView.ps1 on the windows machine
+
 
 def ACCsforKerb(request, agent=''):
     if request.method=='POST':
-        task = 'echo "===============SPN Users===============";import-module powerview;Get-DomainUser -SPN -Properties samaccountname,ServicePrincipalName'
+        task = 'echo "===============SPN Users===============";import-module c:\windows\temp\powerview.ps1;Get-DomainUser -SPN -Properties samaccountname,ServicePrincipalName'
         
         task_path = os.path.normpath(current_path+os.sep+os.pardir+os.sep+os.pardir)+"/data/listeners/agents/{}/tasks".format(agent)
         with open(task_path, "w") as f:
