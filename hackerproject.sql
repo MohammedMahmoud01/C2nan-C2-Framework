deb05bde17e7138bdf6c64b33b5d2038f3e6ca4e
Mohammed Mahmoud AbdEllah <101204054+MohammedMahmoud01@users.noreply.github.com>
2022-12-09 01:26:16 +0200

    update database

--- hackerproject.sql
+++ hackerproject.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 02, 2022 at 04:22 PM
+-- Generation Time: Dec 09, 2022 at 12:25 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.28
 
@@ -128,7 +128,7 @@ CREATE TABLE `auth_user` (
 --
 
 INSERT INTO `auth_user` (`id`, `password`, `last_login`, `is_superuser`, `username`, `first_name`, `last_name`, `email`, `is_staff`, `is_active`, `date_joined`) VALUES
-(1, 'pbkdf2_sha256$390000$0pGk3o3yl73xS2iQenRLp3$hILWC9dCD+Bvkbua6RgVlZZEx21QbQSNxXpf/janPgE=', '2022-12-02 15:15:14.266109', 1, 'admin', '', '', 'admin@gmail.com', 1, 1, '2022-10-20 10:10:02.435570'),
+(1, 'pbkdf2_sha256$390000$0pGk3o3yl73xS2iQenRLp3$hILWC9dCD+Bvkbua6RgVlZZEx21QbQSNxXpf/janPgE=', '2022-12-08 22:56:19.281408', 1, 'admin', '', '', 'admin@gmail.com', 1, 1, '2022-10-20 10:10:02.435570'),
 (2, 'pbkdf2_sha256$390000$VcJyCcXjTw3BLzdoAFOs4l$vzeQ5BNSBrTDnreTIz0Ivyaf8+BHyD0XtSvRGsb8M80=', '2022-10-21 18:01:51.788209', 1, 'tamer', '', '', 'tamer@gmail.com', 1, 1, '2022-10-21 18:00:11.000000'),
 (3, 'pbkdf2_sha256$390000$rBKtztMUtF8v2WViZlwh44$xM7mHlGyOQe+JHSXq2wLKha8Woj7XOpOR4D2aj+zJGY=', NULL, 0, 'rana', '', '', 'rana@gmail.com', 0, 1, '2022-10-21 18:41:06.000000');
 
@@ -187,8 +187,15 @@ CREATE TABLE `blog_listenerform` (
 --
 
 INSERT INTO `blog_listenerform` (`id`, `interface`, `created_date`, `stoped_date`) VALUES
-(12, 'eth0', '2022-11-30 22:23:47.719228', NULL),
-(13, 'eth1', '2022-12-02 15:03:33.343225', NULL);
+(22, 'eth0', '2022-12-03 20:36:37.250243', NULL),
+(23, 'eth0', '2022-12-03 20:38:10.973650', NULL),
+(24, 'eth0', '2022-12-03 20:40:40.764978', NULL),
+(25, 'eth0', '2022-12-03 20:42:15.632449', NULL),
+(26, 'eth0', '2022-12-08 22:10:43.609250', NULL),
+(27, 'eth0', '2022-12-08 22:11:36.135255', NULL),
+(28, 'eth0', '2022-12-08 22:19:16.148775', NULL),
+(29, 'eth0', '2022-12-08 22:19:54.236979', NULL),
+(30, 'eth0', '2022-12-08 22:25:28.555627', NULL);
 
 -- --------------------------------------------------------
 
@@ -223,7 +230,22 @@ INSERT INTO `blog_modules` (`id`, `module_name`, `module_type`, `created_date`) 
 (13, 'Systeminfo', 1, '2022-11-30 18:29:21.325056'),
 (14, 'Updates', 1, '2022-11-30 18:29:46.847832'),
 (15, 'Upload', 1, '2022-11-30 18:29:55.945993'),
-(16, 'WinDefStat', 1, '2022-11-30 18:30:05.703401');
+(16, 'WinDefStat', 1, '2022-11-30 18:30:05.703401'),
+(17, 'Named_pipes', 1, '2022-12-08 16:56:34.506216'),
+(18, 'juicypotato', 1, '2022-12-08 16:57:03.065772'),
+(19, 'Apache', 2, '2022-12-08 17:11:11.724291'),
+(20, 'Available Shell', 2, '2022-12-08 17:11:50.364717'),
+(21, 'Cronjobs', 2, '2022-12-08 17:12:04.265437'),
+(22, 'Hidden Files', 2, '2022-12-08 17:17:38.682779'),
+(23, 'Lsof', 2, '2022-12-08 17:18:48.406120'),
+(24, 'OS info', 2, '2022-12-08 17:19:12.653008'),
+(25, 'Processes', 2, '2022-12-08 17:19:34.693143'),
+(26, 'Selinux', 2, '2022-12-08 17:19:54.149273'),
+(27, 'SpecialPermissions', 2, '2022-12-08 17:20:08.475925'),
+(28, 'TCP_UDP', 2, '2022-12-08 17:20:19.301327'),
+(29, 'usr_priv', 2, '2022-12-08 17:21:20.412562'),
+(30, 'Usrs info', 2, '2022-12-08 17:21:34.429383'),
+(31, 'Whoami', 2, '2022-12-08 17:21:49.652072');
 
 -- --------------------------------------------------------
 
@@ -281,7 +303,32 @@ INSERT INTO `django_admin_log` (`id`, `action_time`, `object_id`, `object_repr`,
 (32, '2022-11-30 22:23:27.151225', '8', 'ListenerForm object (8)', 3, '', 7, 1),
 (33, '2022-11-30 22:23:27.154209', '9', 'ListenerForm object (9)', 3, '', 7, 1),
 (34, '2022-11-30 22:23:27.159208', '10', 'ListenerForm object (10)', 3, '', 7, 1),
-(35, '2022-11-30 22:23:27.162209', '11', 'ListenerForm object (11)', 3, '', 7, 1);
+(35, '2022-11-30 22:23:27.162209', '11', 'ListenerForm object (11)', 3, '', 7, 1),
+(36, '2022-12-08 16:56:34.507216', '17', 'Modules object (17)', 1, '[{\"added\": {}}]', 10, 1),
+(37, '2022-12-08 16:57:03.066785', '18', 'Modules object (18)', 1, '[{\"added\": {}}]', 10, 1),
+(38, '2022-12-08 17:11:11.726291', '19', 'Modules object (19)', 1, '[{\"added\": {}}]', 10, 1),
+(39, '2022-12-08 17:11:50.365903', '20', 'Modules object (20)', 1, '[{\"added\": {}}]', 10, 1),
+(40, '2022-12-08 17:12:04.265437', '21', 'Modules object (21)', 1, '[{\"added\": {}}]', 10, 1),
+(41, '2022-12-08 17:17:38.686461', '22', 'Modules object (22)', 1, '[{\"added\": {}}]', 10, 1),
+(42, '2022-12-08 17:18:48.407119', '23', 'Modules object (23)', 1, '[{\"added\": {}}]', 10, 1),
+(43, '2022-12-08 17:19:12.654009', '24', 'Modules object (24)', 1, '[{\"added\": {}}]', 10, 1),
+(44, '2022-12-08 17:19:34.693143', '25', 'Modules object (25)', 1, '[{\"added\": {}}]', 10, 1),
+(45, '2022-12-08 17:19:54.150275', '26', 'Modules object (26)', 1, '[{\"added\": {}}]', 10, 1),
+(46, '2022-12-08 17:20:08.477130', '27', 'Modules object (27)', 1, '[{\"added\": {}}]', 10, 1),
+(47, '2022-12-08 17:20:19.302327', '28', 'Modules object (28)', 1, '[{\"added\": {}}]', 10, 1),
+(48, '2022-12-08 17:21:20.413562', '29', 'Modules object (29)', 1, '[{\"added\": {}}]', 10, 1),
+(49, '2022-12-08 17:21:34.431386', '30', 'Modules object (30)', 1, '[{\"added\": {}}]', 10, 1),
+(50, '2022-12-08 17:21:49.653070', '31', 'Modules object (31)', 1, '[{\"added\": {}}]', 10, 1),
+(51, '2022-12-08 22:18:54.394908', '12', 'ListenerForm object (12)', 3, '', 7, 1),
+(52, '2022-12-08 22:18:54.398904', '13', 'ListenerForm object (13)', 3, '', 7, 1),
+(53, '2022-12-08 22:18:54.402905', '14', 'ListenerForm object (14)', 3, '', 7, 1),
+(54, '2022-12-08 22:18:54.405905', '15', 'ListenerForm object (15)', 3, '', 7, 1),
+(55, '2022-12-08 22:18:54.409905', '16', 'ListenerForm object (16)', 3, '', 7, 1),
+(56, '2022-12-08 22:18:54.413592', '17', 'ListenerForm object (17)', 3, '', 7, 1),
+(57, '2022-12-08 22:18:54.416547', '18', 'ListenerForm object (18)', 3, '', 7, 1),
+(58, '2022-12-08 22:18:54.420547', '19', 'ListenerForm object (19)', 3, '', 7, 1),
+(59, '2022-12-08 22:18:54.424547', '20', 'ListenerForm object (20)', 3, '', 7, 1),
+(60, '2022-12-08 22:18:54.427549', '21', 'ListenerForm object (21)', 3, '', 7, 1);
 
 -- --------------------------------------------------------
 
@@ -379,8 +426,10 @@ INSERT INTO `django_session` (`session_key`, `session_data`, `expire_date`) VALU
 ('9mu5xksxhxxvgyow4zr76e7j5l31y9qh', '.eJxVjDsOwjAQBe_iGll21r-lpOcMlj9rHEC2FCcV4u4QKQW0b2bei_mwrdVvgxY_Z3Zmkp1-txjSg9oO8j20W-ept3WZI98VftDBrz3T83K4fwc1jPqtFYBIDi1MQlqFGtHSZECqLK2hkqMhJ4pTRusSAaLTAYW1hChUAgjs_QGfdjZx:1oqwCm:kUwZ0hbWFTi6tELkYZs9N6Ryr3jN95y168T5zXOXruo', '2022-11-18 12:53:52.923814'),
 ('ffkmtxohnmj9wox5b4pwd62vq559zb1v', '.eJxVjDsOwjAQBe_iGll21r-lpOcMlj9rHEC2FCcV4u4QKQW0b2bei_mwrdVvgxY_Z3Zmkp1-txjSg9oO8j20W-ept3WZI98VftDBrz3T83K4fwc1jPqtFYBIDi1MQlqFGtHSZECqLK2hkqMhJ4pTRusSAaLTAYW1hChUAgjs_QGfdjZx:1olUXq:N9mYZcQTKkF441xKs8JclFkNfC407UwZzvw5CLmqGQ4', '2022-11-03 12:21:06.188784'),
 ('fjjxjii6eoew1r2gbcxbizyf6iw0sge4', '.eJxVjDsOwjAQBe_iGll21r-lpOcMlj9rHEC2FCcV4u4QKQW0b2bei_mwrdVvgxY_Z3Zmkp1-txjSg9oO8j20W-ept3WZI98VftDBrz3T83K4fwc1jPqtFYBIDi1MQlqFGtHSZECqLK2hkqMhJ4pTRusSAaLTAYW1hChUAgjs_QGfdjZx:1ooAER:hvZ_kl_Jh-FgCuwfW14-fmGGglFuywIX_uXYrjBuP84', '2022-11-10 21:16:07.793420'),
+('hyauwhwr7kkf8pt9foob30y05od2w49h', '.eJxVjDsOwjAQBe_iGll21r-lpErDGSJ_NiQQxVLsVIi740gpoH0zb95s8Hudhr3QNsyJXZlkl98t-Pii9QDp6ddH5jGvdZsDPxR-0sLvOdFyO92_wOTL1N4KQESHFjohrUKNaKkzIFWS1tCYgiEnRqeM1mMACE57FNYSolARwLdopVJbqacls88Xtl46pw:1p3Ppv:d08-crIIiXA8HL3sCW3w4ZpgxVmGROPDCv9fKd40KP4', '2022-12-22 22:57:51.862038'),
+('m2f688bvtn4iffzr6tfetq24r62mftyn', '.eJxVjDsOwjAQBe_iGll21r-lpErDGSJ_NiQQxVLsVIi740gpoH0zb95s8Hudhr3QNsyJXZlkl98t-Pii9QDp6ddH5jGvdZsDPxR-0sLvOdFyO92_wOTL1N4KQESHFjohrUKNaKkzIFWS1tCYgiEnRqeM1mMACE57FNYSolARwLdopVJbqacls88Xtl46pw:1p3PPO:Uqpt1kXfpt-KMpiQl6VVEzWM0smoweZ_m-3w5gjNKHk', '2022-12-22 22:30:26.830553'),
 ('vnab4fh1695xtwpee269b9tinyciw72w', '.eJxVjDsOwjAQBe_iGll21r-lpOcMlj9rHEC2FCcV4u4QKQW0b2bei_mwrdVvgxY_Z3Zmkp1-txjSg9oO8j20W-ept3WZI98VftDBrz3T83K4fwc1jPqtFYBIDi1MQlqFGtHSZECqLK2hkqMhJ4pTRusSAaLTAYW1hChUAgjs_QGfdjZx:1olTI4:vWi9GqL4cENxdjVNG4d-AmN2zhFT11VAtusJF2hIcE4', '2022-11-03 11:00:44.367033'),
-('waidt7adoplb5wj5ll81att2w0fv8944', '.eJxVjDsOwjAQBe_iGll21r-lpOcMlj9rHEC2FCcV4u4QKQW0b2bei_mwrdVvgxY_Z3Zmkp1-txjSg9oO8j20W-ept3WZI98VftDBrz3T83K4fwc1jPqtFYBIDi1MQlqFGtHSZECqLK2hkqMhJ4pTRusSAaLTAYW1hChUAgjs_QGfdjZx:1p17kw:kZDEOM8S1GmKipeEeC8iYycz2mPu-4uCxYrlO-jgoGw', '2022-12-16 15:15:14.274108'),
+('wedkg9k6gkqs0wuvzyl44f7lm2dtpbiw', '.eJxVjDsOwjAQBe_iGll21r-lpErDGSJ_NiQQxVLsVIi740gpoH0zb95s8Hudhr3QNsyJXZlkl98t-Pii9QDp6ddH5jGvdZsDPxR-0sLvOdFyO92_wOTL1N4KQESHFjohrUKNaKkzIFWS1tCYgiEnRqeM1mMACE57FNYSolARwLdopVJbqacls88Xtl46pw:1p3Pkg:su_liNRKV4JIRIO9PZU6ndQHRzqHxz_ZsiPLhE80FOQ', '2022-12-22 22:52:26.564398'),
 ('wswalagvqmxvxhh8n7z4126984gmwyfs', '.eJxVjDsOwjAQBe_iGll21r-lpOcMlj9rHEC2FCcV4u4QKQW0b2bei_mwrdVvgxY_Z3Zmkp1-txjSg9oO8j20W-ept3WZI98VftDBrz3T83K4fwc1jPqtFYBIDi1MQlqFGtHSZECqLK2hkqMhJ4pTRusSAaLTAYW1hChUAgjs_QGfdjZx:1olSuO:0XpsH1tKce17ouHOBos3l7N1oxO6Dyt4Gz9fXGOYV0c', '2022-11-03 10:36:16.138598');
 
 --
@@ -528,19 +577,19 @@ ALTER TABLE `blog_agent`
 -- AUTO_INCREMENT for table `blog_listenerform`
 --
 ALTER TABLE `blog_listenerform`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `blog_modules`
 --
 ALTER TABLE `blog_modules`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `django_admin_log`
 --
 ALTER TABLE `django_admin_log`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
 -- AUTO_INCREMENT for table `django_content_type`
